d955a88ae55dfe974362adfdad96aff59972bdfc
alejandro sosa <alejandrososadev@gmail.com>
2025-06-19 14:59:00 -0400

    Adds middleware for endpoint protection

--- src/application/order-routes.ts
+++ src/application/order-routes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { OrderController } from './order-controller';
 import { orderService } from '../infrastructure';
+import { authenticateJWT } from '../shared/jwt-middleware';
 
 const router = Router();
 const orderController = new OrderController(orderService);
 
 // router.post('/', orderController.createOrder.bind(orderController));
-router.get('/:id', orderController.getOrderById.bind(orderController));
+router.get('/:id', authenticateJWT, orderController.getOrderById.bind(orderController));
 // router.get('/user/:userId', orderController.listOrdersByUser.bind(orderController));
 
 export default router;
